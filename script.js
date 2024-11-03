ef09485536f97d5a1873b6978ca0b40c64e31902
arthenry90 <134339241+Arthenry90@users.noreply.github.com>
2024-11-03 10:58:25 -0300

    Working computer Choice

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 console.log("alola");
+
+function getComputerChoice() {
+    valorRandon = Math.floor(Math.random() * 3);
+    console.log(valorRandon);
+    switch (valorRandon){
+        case 0:
+            return "paper";
+        case 1:
+            return "rock";
+        case 2:
+            return "scissor";
+
+    }
+}
