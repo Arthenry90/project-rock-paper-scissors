86dad116df1c7661ce22e1fe43c6c2551b657b17
arthenry90 <134339241+Arthenry90@users.noreply.github.com>
2024-11-04 00:17:40 -0300

    Working game

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
-console.log("alola");
-
 function getComputerChoice() {
     valorRandon = Math.floor(Math.random() * 3);
-    console.log(valorRandon);
-    switch (valorRandon){
+    switch (valorRandon) {
         case 0:
             return "paper";
         case 1:
@@ -12,4 +9,40 @@ function getComputerChoice() {
             return "scissor";
 
     }
+}
+
+function getHumanChoice() {
+    var choice = prompt();
+    return choice.toLowerCase();
+}
+
+function playRound(getComputerChoice, getHumanChoice) {
+    if (
+        (getComputerChoice == "paper" && getHumanChoice == "rock") ||
+        (getComputerChoice == "rock" && getHumanChoice == "scissor") ||
+        (getComputerChoice == "scissor" && getHumanChoice == "paper")
+    ) {
+        console.log("Computador venceu!!!");
+        computerScore++;
+    } else if (getComputerChoice === getHumanChoice) {
+        console.log("Empate!!!");
+    } else {
+        console.log("Você venceu!!!");
+        humanScore++;
+    }
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playGame(){
+    let times = 0;
+    while (times <= 4) {
+        const humanSelec = getHumanChoice();
+        const computerSelec = getComputerChoice();
+        playRound(computerSelec, humanSelec);
+        times++;
+    }
+    return console.log("O placar da máquina foi: " + computerScore + " E o seu placar foi: " + humanScore);
+    ;
 }
